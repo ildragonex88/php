1fd67fdae99d48ee040ba7aab2483930a343442f
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-08-26 15:12:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ header('Content-type: '.$namefr.'');
 header('Content-Disposition: attachment; filename='.$nameff.'');
 echo $content ^ str_repeat($__password__[0], strlen($content));
 }
-function curl_header_function($ch, $header) {
+function header_function($header) {
 global $__content__;
 $pos = strpos($header, ':');
 if ($pos == false) {
@@ -81,61 +81,67 @@ if ($key != 'Transfer-Encoding') {
 $__content__ .= $key . substr($header, $pos);
 }
 }
-return strlen($header);
 }
-function curl_write_function($ch, $content) {
+function write_function($content) {
 global $__content__;
 if ($__content__) {
 echo_content($__content__);
 $__content__ = '';
 }
 echo_content($content);
-return strlen($content);
 }
 function post() {
 list($method, $url, $headers, $body) = decode_request(file_get_contents('php://input'));
-$header_array = array();
-foreach ($headers as $key => $value) {
-$header_array[] = join('-', array_map('ucfirst', explode('-', $key))).': '.$value;
+$headerheader = '';
+foreach ($headers as $key => $line) {
+$headerheader .= "".$key.": ".$line."\r\n";
 }
-$curl_opt = array();
-$ch = curl_init();
-$curl_opt[CURLOPT_URL] = $url;
+$headerin = array();
 switch (strtoupper($method)) {  
 case 'GET':
 break;
 case 'HEAD':
 case 'OPTIONS':
 case 'TRACE':
-$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+$headerin['method'] = $method;
 break;
 case 'POST':
 case 'PATCH':
 case 'PUT':
 case 'DELETE':
-$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+$headerin['method'] = $method;
 if ($body) {
-$curl_opt[CURLOPT_POSTFIELDS] = $body;
+$headerin['content'] = $body;
 }
 break;
 default:
 echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'Method error ' . $method));
 exit(-1);
 }
-$curl_opt[CURLOPT_HTTPHEADER] = $header_array;
-$curl_opt[CURLOPT_RETURNTRANSFER] = true;
-$curl_opt[CURLOPT_HEADERFUNCTION] = 'curl_header_function';
-$curl_opt[CURLOPT_WRITEFUNCTION]  = 'curl_write_function';
-$curl_opt[CURLOPT_TIMEOUT] = 30;
-$curl_opt[CURLOPT_SSL_VERIFYPEER] = false;
-$curl_opt[CURLOPT_SSL_VERIFYHOST] = false;
-$curl_opt[CURLOPT_IPRESOLVE] = CURL_IPRESOLVE_V4;
-curl_setopt_array($ch, $curl_opt);
-curl_exec($ch);
-curl_close($ch);
-if ($GLOBALS['__content__']) {
-echo_content($GLOBALS['__content__']);
-} 
+//$headerin['protocol_version'] = 1.1;
+$headerin['follow_location'] = false;
+$headerin['header'] = $headerheader;
+$headerin['timeout'] = 30;
+$headerin['ignore_errors'] = 1;
+$stcocr = array('http' => $headerin);
+$context = stream_context_create($stcocr);
+$strea = file_get_contents($url, false, $context);
+foreach ($http_response_header as $header) {
+$pos = strpos($header, ':');
+if ($pos == false) {
+header_function($header);
+header_function("\r\n");
+}
+else {
+$key = join('-', array_map('ucfirst', explode('-', substr($header, 0, $pos))));
+if ($key != 'Transfer-Encoding') {
+header_function($key . substr($header, $pos));
+header_function("\r\n");
+}
+}
+}
+header_function("\r\n"); 
+write_function($strea);
 }
 function get() {
 $f = fopen ('1.tmp','rb');
